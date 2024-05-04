02c9ff8abd583723dd16679cac6703e50761ca31
tomasitobambito <tomgacek0604@gmail.com>
2024-05-04 15:50:11 +0200

    added datagen boilerplate code

--- src/main/java/net/okense/dungeonsreborn/DungeonsRebornDataGenerator.java
+++ src/main/java/net/okense/dungeonsreborn/DungeonsRebornDataGenerator.java
@@ -2,10 +2,17 @@ package net.okense.dungeonsreborn;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.okense.dungeonsreborn.datagen.*;
 
 public class DungeonsRebornDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
--- src/main/java/net/okense/dungeonsreborn/datagen/ModTagsProvider.java
+++
@@ -1,4 +0,0 @@
-package net.okense.dungeonsreborn.datagen;
-
-public class ModTagsProvider {
-}
