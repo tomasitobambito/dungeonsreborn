b535212c19a6d86c4a1d27c703cfc05030f01622
tomasitobambito <tomgacek0604@gmail.com>
2024-05-04 15:09:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dungeons Reborn
+This is a mod that implements blocks and mobs from Minecraft Dungeons into Minecraft.
+
+# Credits
+- Okense
+- tomasitobambito
---
+++ src/main/java/net/okense/dungeonsreborn/DungeonsRebornDataGenerator.java
@@ -0,0 +1,11 @@
+package net.okense.dungeonsreborn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DungeonsRebornDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
